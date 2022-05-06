6e7dd385c714a4d4710c2a3b4591160228b71c0c
Kauan Magaldi <kauanmagaldi29@gmail.com>
2022-05-06 10:58:07 -0300

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,7 @@
-/*
-* Template Name: FlexStart - v1.9.0
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
